7774ac0c4855cc4a44e8636946ef837e68d38ea1
Remi Verhoeven <r.verhoeven@duravermeer.nl>
2025-08-24 16:51:45 +0200

    Fix plugin package: add missing wolwaker.html, fix version/ABI mismatches, update checksum

--- Jellyfin.Plugin.WolWaker/PluginServiceRegistrator.cs
+++ Jellyfin.Plugin.WolWaker/PluginServiceRegistrator.cs
@@ -4,31 +4,66 @@ using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Jellyfin.Plugin.WolWaker.Services;
+using Jellyfin.Plugin.WolWaker.Configuration;
+using Jellyfin.Plugin.WolWaker.Controllers;
+using Jellyfin.Plugin.WolWaker;
+using Jellyfin.Data.Events;
+using Jellyfin.Data.Events.System;
+using Jellyfin.Data.Events.Users;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Playback;
+using MediaBrowser.Model.Entities;
 
 namespace Jellyfin.Plugin.WolWaker;
 
 /// <summary>
-/// Service registrator for the WoL Waker plugin.
+/// Registers plugin services with Jellyfin's dependency injection container.
 /// </summary>
 public class PluginServiceRegistrator : IPluginServiceRegistrator
 {
     /// <summary>
-    /// Registers the plugin services with the dependency injection container.
+    /// Registers plugin services with the service collection.
     /// </summary>
-    /// <param name="serviceCollection">The service collection.</param>
-    /// <param name="applicationHost">The application host.</param>
-    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+    /// <param name="serviceCollection">The service collection to register services with.</param>
+    public void RegisterServices(IServiceCollection serviceCollection)
     {
-        // Register the Wake-on-LAN service
+        // Register core services
         serviceCollection.AddSingleton<WolService>();
-
-        // Register the plugin instance (as a singleton, since it's the main plugin instance)
+        serviceCollection.AddSingleton<PlaybackMonitorService>();
+        serviceCollection.AddScoped<WolController>();
+        
+        // Register the plugin instance
         serviceCollection.AddSingleton<Plugin>();
+        
+        // Register configuration
+        serviceCollection.AddSingleton<PluginConfiguration>();
+    }
 
-        // Register the controller (scoped, as it's typically per-request)
-        serviceCollection.AddScoped<Controllers.WolController>();
+    /// <summary>
+    /// Subscribes to Jellyfin events after the application has started.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider.</param>
+    public void SubscribeToEvents(IServiceProvider serviceProvider)
+    {
+        try
+        {
+            var logger = serviceProvider.GetRequiredService<ILogger<PluginServiceRegistrator>>();
+            var playbackMonitor = serviceProvider.GetRequiredService<PlaybackMonitorService>();
+            var eventManager = serviceProvider.GetRequiredService<IEventManager>();
+            var libraryManager = serviceProvider.GetRequiredService<ILibraryManager>();
 
-        // Logging for service registration is usually handled by the plugin manager itself
-        // or within the services/controllers that have injected loggers.
+            // Subscribe to playback start events
+            eventManager.Subscribe<PlaybackStartEventArgs>(playbackMonitor.HandlePlaybackStartAsync);
+            
+            // Subscribe to library item access events
+            eventManager.Subscribe<ItemAccessEventArgs>(playbackMonitor.HandleItemAccessAsync);
+
+            logger.LogInformation("WoL Waker plugin events subscribed successfully");
+        }
+        catch (Exception ex)
+        {
+            var logger = serviceProvider.GetRequiredService<ILogger<PluginServiceRegistrator>>();
+            logger.LogError(ex, "Failed to subscribe to Jellyfin events");
+        }
     }
 }
