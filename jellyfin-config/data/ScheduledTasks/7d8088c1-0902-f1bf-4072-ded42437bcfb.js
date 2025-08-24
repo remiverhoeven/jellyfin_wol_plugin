95a2ca86d62ae470343d09503e446ea29fc96ef9
Remi Verhoeven <r.verhoeven@duravermeer.nl>
2025-08-24 16:40:21 +0200

    Clean up repository: remove development files, test data, and unnecessary documentation

--- jellyfin-config/data/ScheduledTasks/7d8088c1-0902-f1bf-4072-ded42437bcfb.js
+++
@@ -1,1 +0,0 @@
-{"StartTimeUtc":"2025-08-24T12:56:42.5589312Z","EndTimeUtc":"2025-08-24T12:56:42.5602153Z","Status":"Completed","Name":"Clean Transcode Directory","Key":"DeleteTranscodeFiles","Id":"7d8088c10902f1bf4072ded42437bcfb"}
